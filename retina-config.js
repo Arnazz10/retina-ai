f47bb001dc1d1ff93e89b032359eae88b7336f75
Arnazz10 <arnabmal665@gmail.com>
2026-04-21 11:16:36 +0530

    Finalize static frontend deployment: enabled frontendOnly and ignored backend files

--- retina-config.js
+++ retina-config.js
@@ -1,5 +1,5 @@
 window.RETINA_CONFIG = {
-  frontendOnly: false,
+  frontendOnly: true,
   apiBase: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
     ? "http://127.0.0.1:5000" 
     : "" // On production, assume same origin or configured elsewhere
